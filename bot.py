be7f1f672fb4e0e5b0a7d05dc36cbb5613738e1b
almauj <aalijah.johnson@proton.me>
2026-07-09 14:12:02 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,65 @@
+import random as r
+import numpy as np
+
+class HeuristicBot:
+    def __init__(self, color=1):
+        self.color = color
+
+    def evaluate_move(self, board, row, col, engine):
+        """Calculates numerical weight for specified empty cell.
+
+            Args: 
+                - board - the entire board layout
+                - row, col - the specific empty cell
+                - engine - the game engine
+
+            Returns: 
+                scores - logical weights
+        """
+
+        score = 0
+
+        neighbors = engine.get_neighbors(row,col)
+
+        for n_row, n_col in neighbors:
+            neighbor_tile = board[n_row][n_col]
+
+            if neighbor_tile == 1:
+                score += 8
+            elif neighbor_tile == self.color:
+                score += 5
+            else:
+                pass
+        
+        return score
+    
+    def get_move(self, engine):
+        """Selects best move position according to scores.
+
+            Args: engine - game engine
+
+            Returns: random choice between best moves based on entire board
+        
+        """
+
+        board = engine.board
+        best_score = -1000
+        best_moves = []
+
+        for r in range(engine.size):
+            for c in range(engine.size):
+                if board[r][c] == 0: # empty space
+                    move_score = self.evaluate_move(board, r, c, engine)
+
+                    if move_score > best_score:
+                        best_score = move_score
+                        best_moves.clear()
+                        best_moves.append((r, c))
+
+                    elif move_score == best_score:
+                        best_moves.append((r, c))
+
+                    else:
+                        pass
+
+        return r.choice(best_moves) if best_moves else None
